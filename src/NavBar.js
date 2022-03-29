2705fae27b84102abfa47068fd3047cd99dc1d01
serref jaguraga <reef163@gmail.com>
2022-03-29 15:18:03 -0400

    added image

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,14 +1,15 @@
 import React from "react";
 
-function Navbar() {
+function Nav() {
   return (
-  <>
-  
-  <h1>Welcome to Sails!</h1>
-
-  </>
-  )
-
+    <nav>
+      <h3>Logo</h3>
+      <ul className="nav-links">
+        <li>Home</li>
+        <li>About</li>
+      </ul>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Nav;
